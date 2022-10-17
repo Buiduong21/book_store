3e5f0c09426a1d0a0e9f9467b49be031233c2222
Buiduong21 <duongbui2110@gmail.com>
2022-10-17 09:31:41 +0700

    Init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $n = 642386;
+    $str = strval($n);
+    $arr1 = str_split($str);
+    
+    // var_dump($str);
+
+    echo "<pre>";
+        print_r($arr1);
+    echo " </pre>";
+
+?>
