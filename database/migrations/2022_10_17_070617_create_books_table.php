586ef26cfddf1e6f1a82d67e438fa4cc750464b4
Buiduong21 <duongbui2110@gmail.com>
2022-10-17 17:07:07 +0700

    create_migrate_and_delete_file_welcome

---
+++ database/migrations/2022_10_17_070617_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float('price');
+            $table->float('sale_price');
+            $table->date('year_manufacturing');
+            $table->integer('category_id')->unsigned();
+            $table->integer('author_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('author_id')->references('id')->on('authors');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
